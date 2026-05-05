0bbe206d809279dbf38330dc977bcf7da71e6972
tajiknomi <tajiknomi@gmail.com>
2026-05-05 09:42:58 +0500

    before std::wstring

--- Project1/include/extractor.h
+++ Project1/include/extractor.h
@@ -7,9 +7,15 @@
 
 class ForensicExtractor {
 
+private:
+    std::vector<std::tuple<std::string, std::string>> getFilePathsFromJson(const std::string& jsonFilePath);
+    std::filesystem::path createDirForConnectedDevice(const std::string& input);
+
+private:
+    ADB& adb;
+
 public:
     ForensicExtractor(ADB& adb);
-
     int listDevices();
     int extractSMS(const std::string& outputFileName);
     int extractCallLogs(const std::string& outputFileName);
@@ -17,11 +23,4 @@ public:
     int extractMediaStoreDb(const std::string& outputFileName);
     int extractDeviceInfo(const std::string& outputFileName);
     int extractUserInstalledAppsList(const std::string& outputFileName);
-
-private:
-    std::vector<std::tuple<std::string, std::string>> getFilePathsFromJson(const std::string& jsonFilePath);
-    std::filesystem::path createDirForConnectedDevice(const std::string& input);
-
-private:
-    ADB& adb;
 };
--- Project1/src/main.cpp
+++ Project1/src/main.cpp
@@ -1,15 +1,40 @@
 #include <iostream>
+#include <Windows.h>
 #include "adb.h"
 #include "extractor.h"
 #include "prompts.h"
 
 
+std::string FindAdbPath(void)
+{
+    char buffer[MAX_PATH];
+
+    // Search in current directory + PATH automatically
+    unsigned long result = SearchPathA(
+        nullptr,        // search in standard locations (current dir + PATH)
+        "adb.exe",      // file to find
+        nullptr,        // no extension
+        MAX_PATH,
+        buffer,
+        nullptr
+    );
+
+    if (result > 0 && result < MAX_PATH)
+        return std::string(buffer);
+
+    return std::string("");
+}
+
 int main() {
-    ADB adb("adb"); // or full path: "C:\\platform-tools\\adb.exe"
+
+    std::string pathToAdb = FindAdbPath();
+    if (pathToAdb.empty()) {
+        std::cerr << "Couldn't found adb.exe on your system, try adding adb.exe to you PATH environment variable" << std::endl;
+        return -1;
+    }
+    ADB adb(pathToAdb); // or full path: "C:\\platform-tools\\adb.exe"
     ForensicExtractor extractor(adb);
-
     showAndroidAdbSetup();
-
     int choice;
 
     while (true) {
