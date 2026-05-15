66539fe9d72fd9ed5c36693b93520994026292df
tajiknomi <tajiknomi@gmail.com>
2026-05-15 14:50:26 +0500

    Removed SQLite, minizip and zip folders

--- include/zlib/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/illusion/Downloads/zlib-1.3.2/zlib-1.3.2
-# Build directory: C:/Users/illusion/Downloads/zlib-1.3.2/zlib-1.3.2/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("test")
-subdirs("contrib")
--- include/zlib/build/contrib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/illusion/Downloads/zlib-1.3.2/zlib-1.3.2/contrib
-# Build directory: C:/Users/illusion/Downloads/zlib-1.3.2/zlib-1.3.2/build/contrib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
